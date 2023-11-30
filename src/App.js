8b0aa29e947ec296357e84faecb2e7ac87dcc130
Juan Aguilera <73394216+JuanGdev@users.noreply.github.com>
2023-11-30 04:16:11 +0000

    Hotfix handleFunctions not defined

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleImageAnalysis = () => {
+    // Add your logic for image analysis here
+  };
+
+  const handleImageGeneration = () => {
+    // Add your logic for image generation here
+  };
+
   return (
     <div>
       <h1>Computer Vision</h1>
-      <input type="text" placeholder="Enter image URL or prompt" />
+      <input
+        type="text"
+        placeholder="Enter image URL or prompt"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
       <button onClick={handleImageAnalysis}>Analyze Image</button>
       <button onClick={handleImageGeneration}>Generate Image</button>
     </div>
